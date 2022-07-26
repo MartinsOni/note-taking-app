822cb95c5ba28cd35ff911fc9dd7f72a93105d1a
Martins Oni <kayobola2004@yahoo.com>
2022-07-26 22:47:51 +0200

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,11 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} >
             <Route path='/list' element={<NoteList list={list} setList={setList} />}>
-              {/* <Route index element={<main style={{ padding: "2rem" }}><h2>Title and Body</h2></main>} /> */}
+              <Route index element={<main style={{ padding: "2rem" }}><h2>Title and Body</h2></main>} />
               <Route path='/list/:id' element={<NoteView list={list} setList={setList}  />} />
-            </Route>
-            <Route path="*" element={<Home />} />
+            </Route>  
           </Route>
+          <Route path="*" element={<Home />} />
         </Routes>
 
       </BrowserRouter>
